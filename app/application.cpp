d8f3ed23be7e76d593670e5f7b12c640044f86be
verybadsoldier <vbs@springrts.de>
2018-06-10 14:46:03 +0200

    do not disable debug output by default

--- app/application.cpp
+++ app/application.cpp
@@ -28,7 +28,6 @@ Application app;
 void GDB_IRAM_ATTR init() {
 
     Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
-    Serial.systemDebugOutput(false); // don`t show system debug messages
     //System.setCpuFrequencye(CF_160MHz);
 
     // set CLR pin to input
