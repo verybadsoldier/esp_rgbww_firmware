4f69eab2d55493974e34cad8426f618003fee27f
verybadsoldier <vbs@springrts.de>
2019-07-13 11:09:59 +0200

    Update create_info.sh

--- .travis/create_info.sh
+++ .travis/create_info.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-TIMESTAMP=$(date "+%Y-%m-%d-%H:%M:%S")
-echo "Timestamp: $TIMESTAMP\nCommit: $TRAVIS_COMMIT\nBranch: $TRAVIS_BRANCH" > $1
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+echo "Build Timestamp: $TIMESTAMP\nCommit: $TRAVIS_COMMIT\nBranch: $TRAVIS_BRANCH" > $1
