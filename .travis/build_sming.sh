fb8c06a9ed20b2fa2142a50426b99ba0eb4ed751
verybadsoldier <vbs@springrts.de>
2019-07-14 19:07:02 +0200

    travis...

--- .travis/build_sming.sh
+++ .travis/build_sming.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-BUILD_DIR=$1
-ESP_OPEN_SDK_BIN=$2
-
 echo "Downloading esp-open-sdk from $ESP_OPEN_SDK_BIN"
 wget --no-verbose $ESP_OPEN_SDK_BIN
 echo "Extracting..."
 tar -Jxvf esp-open-sdk.tar.xz
 ls esp-open-sdk
-export ESP_HOME=$BUILD_DIR/esp-open-sdk
-export SMING_HOME=$BUILD_DIR/Sming/Sming
-export SDK_BASE=$BUILD_DIR/Sming/Sming/third-party/ESP8266_NONOS_SDK
+export ESP_HOME=$TRAVIS_BUILD_DIR/esp-open-sdk
+export SMING_HOME=$TRAVIS_BUILD_DIR/Sming/Sming
+export SDK_BASE=$TRAVIS_BUILD_DIR/Sming/Sming/third-party/ESP8266_NONOS_SDK
 cd $SMING_HOME
 echo "Removing Sming examples..."
 rm -rf samples
