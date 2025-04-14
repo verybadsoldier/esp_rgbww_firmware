27f68e6487f0f5d6025969ede0fdf4ab18c93f47
Peter Jakobs <pjakobs@mobileme.de>
2025-04-14 19:01:07 +0200

    removed old api code from webserver

--- include/webserver.h
+++ include/webserver.h
@@ -93,19 +93,17 @@ private:
     void sendApiResponse(HttpResponse &response, JsonObjectStream* stream, HttpStatus code = HTTP_STATUS_OK);
     void sendApiCode(HttpResponse &response, API_CODES code, String msg = "");
 
-    void onUpload(HttpRequest &request, HttpResponse &response);
-    void onObject(HttpRequest &request, HttpResponse &response);
     bool checkHeap(HttpResponse &response);
 
+    String makeId();
+    
     static bool isPrintable(String& str);
-
-    void onStorage(HttpRequest &request, HttpResponse &response);
 
     void setCorsHeaders(HttpResponse &response);
 
     void wsConnected(WebsocketConnection& socket);
     void wsDisconnected(WebsocketConnection& socket);
-    String makeId();
+    
 };
 
 #endif // APP_WEBSERVER_H_
